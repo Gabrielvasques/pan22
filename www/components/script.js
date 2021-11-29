9576b0f04d93d0f096515f2e70040d6036f0244e
Gabrielvasques <gabrielvasques265@gmail.com>
2021-11-30 00:00:32 +0900

    Project's Initial Commit

---
+++ www/components/script.js
@@ -0,0 +1,31 @@
+// This is a JavaScript file
+
+$(document).on('click', '#q1', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag2.html";
+});
+$(document).on('click', '#q2', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag3.html";
+});
+$(document).on('click', '#q3', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag4.html";
+});
+$(document).on('click', '#q4', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag5.html";
+});
+$(document).on('click', '#q5', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag6.html";
+});
+$(document).on('click', '#0', function(){
+  alert("Resposta Errada! Tente Novamente!");
+});
+
+//$(document).on('click', '#q1', function(){
+  //if(correto = 1){
+  //  window.location = "pag2.html";
+  //}
+//});
